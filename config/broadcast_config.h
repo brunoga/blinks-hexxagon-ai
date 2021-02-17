9a19bae9e03c7c9f21d395b982852cd34a5f4bd8
Bruno Albuquerque <bga@gmail.com>
2021-02-17 15:27:14 -0800

    Update broadcast config to match Hexxagon.
    - No effect on the AI though as we do not use broadcasts.

--- config/broadcast_config.h
+++ config/broadcast_config.h
@@ -4,6 +4,10 @@
 // This is the maximum payload we will need.
 #define BROADCAST_MESSAGE_PAYLOAD_BYTES 4
 
+// Disable message replies. Saves considerable space.
 #define BROADCAST_DISABLE_REPLIES
+
+// Enable message handler support. Required by the mapping code.
+#define BROADCAST_ENABLE_MESSAGE_HANDLER
 
 #endif
