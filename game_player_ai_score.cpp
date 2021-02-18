a7ab5fdcf377a3f7790b34258749a4bba1056e0c
Bruno Albuquerque <bga@gmail.com>
2021-02-18 08:27:20 -0800

    Add some comments for the levl 5 AI.

--- game_player_ai_score.cpp
+++ game_player_ai_score.cpp
@@ -38,21 +38,38 @@ bool GetMove(position::Coordinates* origin, position::Coordinates* target) {
                               &target_total_neighbors, &target_player_neighbors,
                               &target_enemy_neighbors);
 
+    // Are we moving to a border position?
     bool target_is_border = (target_total_neighbors < FACE_COUNT);
+
+    // Are we duplicating or moving (jumping)?
     bool target_is_jump =
         (position::coordinates::Distance(*origin, *target) > 1);
 
+    // This is the main component of our score. The higher the number of enemies
+    // we capture, the better.
     int16_t target_enemies_score =
         target_enemy_neighbors * GAME_PLAYER_AI_SCORE_STANDARD_MULTIPLIER;
+
+    // We prefer clusters, so moving near other positions we control is also
+    // good.
     int16_t target_player_score =
         target_player_neighbors * GAME_PLAYER_AI_SCORE_JOIN_MULTIPLIER;
+
+    // Score for keeping the origin position. Currently we get a fixed positive
+    // score if we decide to not jump or a negative score based on the number
+    // of player neighboors we are leaving behind otherwise.
     int16_t origin_player_score =
         (target_is_jump ? -(origin_player_neighbors *
                             GAME_PLAYER_AI_SCORE_LEAVE_MULTIPLIER)
                         : GAME_PLAYER_AI_SCORE_STANDARD_MULTIPLIER);
+
+    // Borders are better than no borders, so we give a small score boost if we
+    // duplicate/move to a border.
     int16_t target_border_score =
         (target_is_border ? GAME_PLAYER_AI_SCORE_BORDER_SCORE : 0);
 
+    // Final score is just a sum of the scores above. Lots of room for tweaking
+    // here.
     int16_t score = target_enemies_score + target_player_score +
                     origin_player_score + target_border_score;
 
