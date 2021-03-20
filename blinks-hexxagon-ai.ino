39785e5969b6548bdc80f5f7d9751f7d9c9059e0
Bruno <bga@gmail.com>
2021-03-19 17:09:43 -0700

    Swithc level selection and player selection.
    - Ai selection is entered by single click now. Single click again cycle
      through levels.
    - Double-click cycles through players.

--- blinks-hexxagon-ai.ino
+++ blinks-hexxagon-ai.ino
@@ -44,19 +44,19 @@ void loop() {
 
     game::state::Set(state);
   } else {
-    if (button_double_clicked) {
-      blink::state::StartLevelSelection();
-    } else if (button_clicked) {
+    if (button_clicked) {
       if (blink::state::GetLevelSelection()) {
         blink::state::NextAILevel();
         blink::state::StartLevelSelection();
       } else {
-        // Switch to next available player.
-        byte next_player = game::player::GetNext(blink::state::GetPlayer());
-
-        // Make sure we filter out the empty player.
-        blink::state::SetPlayer(next_player == 0 ? 1 : next_player);
+        blink::state::StartLevelSelection();
       }
+    } else if (button_double_clicked) {
+      // Switch to next available player.
+      byte next_player = game::player::GetNext(blink::state::GetPlayer());
+
+      // Make sure we filter out the empty player.
+      blink::state::SetPlayer(next_player == 0 ? 1 : next_player);
     }
   }
 
